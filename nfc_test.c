bb4b7aef2ecbf279c97d52738d4247d45e797894
GetGitGo <yu.jingtao@qq.com>
2015-11-07 10:19:49 +0800

    remove write, add ioctl write. addr_l and addr_h configurable during runtime

--- nfc_test.c
+++ nfc_test.c
@@ -14,12 +14,12 @@
 #define BUFFER_SIZE 1024*1024
 char buffer[BUFFER_SIZE];
 
+NFC_IOC_WRITE_S ioc_write_arg; 
 
 int main(int argc , char* argv[])
 {
     int fd = -1;
     int ret = 0;
-    int size;
     int req;
 
 
@@ -29,81 +29,20 @@ int main(int argc , char* argv[])
         return -1;
     }
 
-#if 1 //pass
-    size = 2048 - 0x14;
-    ret = write( fd, buffer, size);
-    dbgout("write size:0x%x ret:0x%x\n", size, ret);
-#endif
+    ioc_write_arg.data = buffer;
+    ioc_write_arg.size = 2048 - 0x14; 
 
-#if 0 //pass
-    size = 2048 - 4;
-    ret = write( fd, buffer, size);
-    dbgout("write size:0x%x ret:0x%x\n", size, ret);
-    size = 2048;
-    ret = write( fd, buffer, size);
-    dbgout("write size:0x%x ret:0x%x\n", size, ret);
-    size = 2048 + 0x55;
-    ret = write( fd, buffer, size);
-    dbgout("write size:0x%x ret:0x%x\n", size, ret);
-#endif
+    /* About addr_l and addr_h: */
+    /* According to the manual, the low 16bits of addr_l are for 
+       inside page address,so, pay attention when giving values 
+       of both variables */
+    ioc_write_arg.addr_l = 0x10000; 
+    ioc_write_arg.addr_h = 0; 
 
-
-#if 0 //pass
-    size = 4096 - 0x28;
-    ret = write( fd, buffer, size);
-    dbgout("write size:0x%x ret:0x%x\n", size, ret);
-    size = 4096 - 0x4;
-    ret = write( fd, buffer, size);
-    dbgout("write size:0x%x ret:0x%x\n", size, ret);
-    size = 4096;
-    ret = write( fd, buffer, size);
-    dbgout("write size:0x%x ret:0x%x\n", size, ret);
-    size = 4096 + 0x77;
-    ret = write( fd, buffer, size);
-    dbgout("write size:0x%x ret:0x%x\n", size, ret);
-#endif
- 
-#if 0 //pass
-    size = 8192 - 0x88;
-    ret = write( fd, buffer, size);
-    dbgout("write size:0x%x ret:0x%x\n", size, ret);
-    size = 8192 - 0x4;
-    ret = write( fd, buffer, size);
-    dbgout("write size:0x%x ret:0x%x\n", size, ret);
-    size = 8192;
-    ret = write( fd, buffer, size);
-    dbgout("write size:0x%x ret:0x%x\n", size, ret);
-    size = 8192 + 0x99;
-    ret = write( fd, buffer, size);
-    dbgout("write size:0x%x ret:0x%x\n", size, ret);
-#endif
-
-#if 0 //pass
-    size = 1024*128 - 0x12345;
-    ret = write( fd, buffer, size);
-    dbgout("write size:0x%x ret:0x%x\n", size, ret);
-    size = 1024*128 + 0x111;
-    ret = write( fd, buffer, size);
-    dbgout("write size:0x%x ret:0x%x\n", size, ret);
-#endif
-
-#if 0
-    size = 1024*512;
-    ret = write( fd, buffer, size);
-    dbgout("write size:0x%x ret:0x%x\n", size, ret);
-    size = 1024*512 + 0x333;
-    ret = write( fd, buffer, size);
-    dbgout("write size:0x%x ret:0x%x\n", size, ret);
-#endif
-
-#if 0
-    size = 1024*1024;
-    ret = write( fd, buffer, size);
-    dbgout("write size:0x%x ret:0x%x\n", size, ret);
-    size = 1024*1024 - 0x555;
-    ret = write( fd, buffer, size);
-    dbgout("write size:0x%x ret:0x%x\n", size, ret);
-#endif
+    req = NFC_IOC_WRITE;
+    ret = ioctl( fd, req, &ioc_write_arg); 
+    dbgout("ioctl write size:0x%x ret:0x%x\n", 
+        ioc_write_arg.size, ret);
 
 #if 0 
     req = NFC_IOC_CMD_RESET;
